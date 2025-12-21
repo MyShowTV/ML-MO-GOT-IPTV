f1e056a6d5c9eb911be58411ff6d0d6b0cd17a24
MyShowTV <52018654+MyShowTV@users.noreply.github.com>
2025-12-21 14:38:12 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -22,7 +22,7 @@ export default {
     const res = await fetch(m3u8Url, {
       headers: {
         "Referer": "https://www.ofiii.com/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
       }
     });
     let text = await res.text();
